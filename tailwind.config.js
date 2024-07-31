c8f1bac330a210966bdf3a534782f60a249dfb79
= <hadibigdelitaheri.gmail.com>
2024-07-31 23:53:02 +0330

    version 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode : 'jit' ,
   content: ["./index.html"],
   theme: {
     extend: {},
+    screens: {
+        'sm' : {'max' : '480px'} ,
+        'md' : {  'max' : '600px'} ,
+        'lg' : {'max' : '800px'} ,
+        'xl' : {'max' : '1200px'} ,
+        '2xl' : {'max' : '1480px'} ,
+    },
   },
   plugins: [],
-}
-
+};
