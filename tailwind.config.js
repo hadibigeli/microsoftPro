638c32d2771120de064c5e277594e255e7412392
= <hadibigdelitaheri.gmail.com>
2024-08-01 02:37:02 +0330

    version 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode : 'jit' ,
+  mode: "jit",
   content: ["./index.html"],
   theme: {
     extend: {},
     screens: {
-        'sm' : {'max' : '480px'} ,
-        'md' : {  'max' : '600px'} ,
-        'lg' : {'max' : '800px'} ,
-        'xl' : {'max' : '1200px'} ,
-        '2xl' : {'max' : '1480px'} ,
+      sm: { max: "485px" },
+      md: { max: "600px" },
+      lg: { max: "800px" },
+      xl: { max: "1200px" },
+      "2xl": { max: "1480px" },
     },
   },
   plugins: [],
