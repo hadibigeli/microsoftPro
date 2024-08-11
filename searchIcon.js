dce3f2db3a7dea3d3614532cd8c686b5971739e6
= <hadibigdelitaheri.gmail.com>
2024-08-11 19:24:09 +0330

    version 5

---
+++ searchIcon.js
@@ -0,0 +1,27 @@
+let search = document.getElementById("search");
+let xxlMenu = document.getElementById("xxlSizeMenu");
+let inputWide = document.getElementById("inputWide");
+let headerPart = document.getElementById("headerPart");
+let cancel = document.getElementById("cancel");
+const xlSize = 1028;
+
+search.addEventListener("click", () => {
+  xxlMenu.style.display = "none";
+  inputWide.style.display = "flex";
+  headerPart.appendChild("inputWidth");
+});
+
+const onResizeEvent = () => {
+  if (window.innerWidth < xlSize) {
+    inputWide.style.display = "none";
+    xxlMenu.style.display = "flex";
+  }
+  console.log(window.innerWidth);
+};
+
+window.addEventListener("resize", onResizeEvent, true);
+
+cancel.addEventListener("click", () => {
+  xxlMenu.style.display = "flex";
+  inputWide.style.display = "none";
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
+  
   content: ["./index.html"],
   theme: {
     extend: {},
+    fontSize: {
+      'sm': '0.7rem',
+      'base': '1rem',
+      'xl': '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
     screens: {
-      sm: { max: "485px" },
-      md: { max: "600px" },
-      lg: { max: "800px" },
-      xl: { max: "1200px" },
-      "2xl": { max: "1480px" },
+      'sm' : {'max' : "640px"},
+      'md': {'min':'640px' , 'max':'768px'},
+      'lg': {'min':'768px' , 'max':'1024px'},
+      'xl': {'min':'1024px' , 'max':'1280px'},
+      'xxl': {'min': '1280px'}
     },
   },
   plugins: [],
